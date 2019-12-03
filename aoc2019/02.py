9204cde7eb29ca1d303fc04e321f452fe70d7aed
martingaston <35241716+martingaston@users.noreply.github.com>
2019-12-03 22:56:37 +0000

    add test for single opcode 99

--- aoc2019/02.py
+++ aoc2019/02.py
@@ -1,6 +1,18 @@
 import unittest
 from utils import read_file_to_list
 
+def process_intcode(intcode):
+    return [99]
+
 if __name__ == '__main__':
     intcode = read_file_to_list("input/02.txt")[0].split(",")
     print(intcode)
+
+class Test(unittest.TestCase):
+    def test_setup_properly(self):
+        self.assertEqual(10, 5 + 5)
+
+    def test_opcode_of_99_halts(self):
+        result = process_intcode([99])
+
+        self.assertEqual([99], result)
