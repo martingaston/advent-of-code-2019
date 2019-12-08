aa20e569e530539854169c24ae41e2f0e2a3bfb9
martingaston <35241716+martingaston@users.noreply.github.com>
2019-12-08 14:53:47 +0000

    add solution for day 4, part 2

--- aoc2019/04.py
+++ aoc2019/04.py
@@ -6,28 +6,48 @@ INPUT = range(145852, 616943)  # need to +1 from AoC input as Python range is ex
 
 class Password:
     @staticmethod
-    def is_valid(password):
+    def is_valid_part_one(password):
         return (
             Password._has_six_digits(password)
-            and Password._has_two_adjacent_digits(password)
+            and Password._has_at_least_two_adjacent_digits(password)
             and Password._digits_do_not_decrease(password)
         )
 
     @staticmethod
-    def count_valid(passwords):
-        return sum(Password._validate_iter(passwords))
+    def is_valid_part_two(password):
+        return (
+            Password._has_six_digits(password)
+            and Password._has_only_two_adjacent_digits(password)
+            and Password._digits_do_not_decrease(password)
+        )
 
-    def _validate_iter(passwords):
+    @staticmethod
+    def count_valid_part_one(passwords):
+        return sum(Password._validate_iter(passwords, Password.is_valid_part_one))
+
+    @staticmethod
+    def count_valid_part_two(passwords):
+        return sum(Password._validate_iter(passwords, Password.is_valid_part_two))
+
+    def _validate_iter(passwords, validator):
         for password in passwords:
-            yield Password.is_valid(password)
+            yield validator(password)
 
     def _has_six_digits(password):
         return len(str(password)) == 6
 
-    def _has_two_adjacent_digits(password):
+    def _has_at_least_two_adjacent_digits(password):
         password_digits = map(lambda x: int(x), str(password))
         for _, consequtive_digits in itertools.groupby(password_digits):
             if len(list(consequtive_digits)) > 1:
+                return True
+
+        return False
+
+    def _has_only_two_adjacent_digits(password):
+        password_digits = map(lambda x: int(x), str(password))
+        for _, consequtive_digits in itertools.groupby(password_digits):
+            if len(list(consequtive_digits)) == 2:
                 return True
 
         return False
@@ -47,7 +67,10 @@ class Password:
 if __name__ == "__main__":
     print("DAY 4")
     print(
-        f"Part 1: There are {Password.count_valid(INPUT)} valid passwords in the input"
+        f"Part 1: There are {Password.count_valid_part_one(INPUT)} valid passwords in the input"
+    )
+    print(
+        f"Part 2: There are {Password.count_valid_part_two(INPUT)} valid passwords in the input"
     )
 
 
@@ -56,39 +79,48 @@ class Test(unittest.TestCase):
         self.assertEqual(15 % 5, 0)
 
     def test_password_contains_six_digits(self):
-        self.assertFalse(Password.is_valid(12345))
-        self.assertTrue(Password.is_valid(111111))
-        self.assertFalse(Password.is_valid(1234567))
+        self.assertFalse(Password.is_valid_part_one(12345))
+        self.assertTrue(Password.is_valid_part_one(111111))
+        self.assertFalse(Password.is_valid_part_one(1234567))
 
     def test_password_has_two_adjacent_digits(self):
-        self.assertFalse(Password.is_valid(123456))
-        self.assertTrue(Password.is_valid(122345))
+        self.assertFalse(Password.is_valid_part_one(123456))
+        self.assertTrue(Password.is_valid_part_one(122345))
 
     def test_digits_do_not_decrease(self):
-        self.assertFalse(Password.is_valid(223450))
-        self.assertTrue(Password.is_valid(123455))
+        self.assertFalse(Password.is_valid_part_one(223450))
+        self.assertTrue(Password.is_valid_part_one(123455))
 
     def test_111111_is_a_valid_password(self):
         password = 111111
 
-        self.assertTrue(Password.is_valid(password))
+        self.assertTrue(Password.is_valid_part_one(password))
 
     def test_123789_is_not_a_valid_password(self):
         password = 123789  # no double
 
-        self.assertFalse(Password.is_valid(password))
+        self.assertFalse(Password.is_valid_part_one(password))
 
     def test_223450_is_not_a_valid_password(self):
         password = 223450  # decreases
 
-        self.assertFalse(Password.is_valid(password))
+        self.assertFalse(Password.is_valid_part_one(password))
 
     def test_count_valid_passwords(self):
         passwords = [111111, 111112, 111113, 654321]
 
-        self.assertEqual(3, Password.count_valid(passwords))
+        self.assertEqual(3, Password.count_valid_part_one(passwords))
 
     def test_count_valid_password_range(self):
         passwords = range(111111, 111115)
 
-        self.assertEqual(4, Password.count_valid(passwords))
+        self.assertEqual(4, Password.count_valid_part_one(passwords))
+
+    def test_112233_is_valid_for_part_two(self):
+        self.assertTrue(Password.is_valid_part_two(112233))
+
+    def test_123444_is_not_valid_for_part_two(self):
+        self.assertFalse(Password.is_valid_part_two(123444))
+
+    def test_111122_is_valid_for_part_two(self):
+        self.assertTrue(Password.is_valid_part_two(111122))
