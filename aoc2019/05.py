83b46c32f1bed08e596a0a64ecaf9825c2fd970e
martingaston <35241716+martingaston@users.noreply.github.com>
2019-12-12 12:09:36 +0000

    add initial solution to handle immediate mode in multiplication opcodes

--- aoc2019/05.py
+++ aoc2019/05.py
@@ -34,7 +34,7 @@ class Instruction:
         return Operation(int(instruction_slice))
 
     def _parse_parameter_modes(instruction):
-        return [int(i) for i in str(instruction)[::-1][2:]]
+        return [Mode(int(i)) for i in str(instruction)[::-1][2:]]
 
 
 def process_intcode(intcode, intcode_input=sys.stdin, intcode_output=sys.stdout):
@@ -54,11 +54,25 @@ def process_intcode(intcode, intcode_input=sys.stdin, intcode_output=sys.stdout)
             intcode[target] = intcode[augend] + intcode[addend]
             pointer += 4
         elif instruction.operation == Operation.MULTIPLICATION:
-            multiplier = intcode[pointer + 1]
-            multiplicand = intcode[pointer + 2]
+            if (
+                len(instruction.parameter_modes) < 1
+                or instruction.parameter_modes[0] is Mode.POSITION
+            ):
+                multiplier = intcode[intcode[pointer + 1]]
+            elif instruction.parameter_modes[0] is Mode.IMMEDIATE:
+                multiplier = intcode[pointer + 1]
+
+            if (
+                len(instruction.parameter_modes) < 2
+                or instruction.parameter_modes[1] is Mode.POSITION
+            ):
+                multiplicand = intcode[intcode[pointer + 2]]
+            elif instruction.parameter_modes[1] is Mode.IMMEDIATE:
+                multiplicand = intcode[pointer + 2]
+
             target = intcode[pointer + 3]
 
-            intcode[target] = intcode[multiplier] * intcode[multiplicand]
+            intcode[target] = multiplier * multiplicand
             pointer += 4
         elif instruction.operation == Operation.INPUT:
             target = intcode[pointer + 1]
@@ -142,12 +156,16 @@ class Test(unittest.TestCase):
         self.assertEqual(Operation.ADDITION, instruction_addition.operation)
         self.assertEqual(Operation.MULTIPLICATION, instruction_multiplication.operation)
 
-
     def test_opcode_class_can_parse_parameter_mode(self):
         instruction = Instruction.from_int(1002)
 
-        self.assertEqual(instruction.parameter_modes, [0, 1])
+        self.assertEqual(instruction.parameter_modes, [Mode.POSITION, Mode.IMMEDIATE])
         self.assertEqual(instruction.operation, Operation.MULTIPLICATION)
 
     def test_opcode_parameter_that_writes_will_not_be_in_immediate_mode(self):
         pass
+
+    def test_instruction_can_process_immediate_mode(self):
+        result = process_intcode([1002, 4, 3, 4, 33])
+
+        self.assertEqual([1002, 4, 3, 4, 99], result)
